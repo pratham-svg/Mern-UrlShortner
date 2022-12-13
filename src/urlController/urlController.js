96244dbe24502e98835c2555d2db46793e375d48
dk7696822 <dk7696822@gmail.com>
2022-12-13 11:15:12 +0530

    await

--- src/urlController/urlController.js
+++ src/urlController/urlController.js
@@ -10,7 +10,7 @@ exports.urlShortner = async (req, res) => {
     }
     const urlCode = nanoid();
     const shortUrl = `http://localhost:3000/${urlCode}`;
-    const url = urlModel.create({ urlCode, longUrl, shortUrl });
+    const url = await urlModel.create({ urlCode, longUrl, shortUrl });
     return res.status(201).send({ status: true, data: url });
   } catch (err) {
     return errorHandler();
@@ -19,7 +19,7 @@ exports.urlShortner = async (req, res) => {
 
 exports.urlRedirect = async (req, res) => {
   try {
-    const url = urlModel.findOne({ urlCode: req.params.urlCode });
+    const url = await urlModel.findOne({ urlCode: req.params.urlCode });
     if (!url) {
       return res.status(404).send({ status: false, message: "URL not found" });
     }
