1de8a6e689eddc21a81e4cb3c84ce93584accd9b
dk7696822 <dk7696822@gmail.com>
2022-12-14 16:36:44 +0530

    Caching implemented

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const route = require("./routes/route");
 const app = express();
 app.use(express.json());
-
 mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_DB, {
@@ -17,5 +16,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
 });
-
-
--- src/urlController/urlController.js
+++ src/urlController/urlController.js
@@ -1,6 +1,26 @@
 const urlModel = require("../urlModel/urlModel");
 const shortid = require("shortid");
 const errorHandler = require("../errorHandler/errorHandler");
+const redis = require("redis");
+const { promisify } = require("util");
+const redisClient = redis.createClient(
+  18986,
+  "redis-18986.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("YAfvnwb6Nt09XfCkCZGwbMllm7qNZzC9", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//2. Prepare the functions for each command
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 exports.urlShortner = async (req, res) => {
   try {
     const longUrl = req.body.longUrl;
@@ -19,10 +39,15 @@ exports.urlShortner = async (req, res) => {
 
 exports.urlRedirect = async (req, res) => {
   try {
+    let cachedUrl = await GET_ASYNC(`${req.params.urlCode}`);
+    if (cachedUrl) {
+      return res.status(302).send({ status: true, data: cachedUrl });
+    }
     const url = await urlModel.findOne({ urlCode: req.params.urlCode });
     if (!url) {
       return res.status(404).send({ status: false, message: "URL not found" });
     }
+    await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(url));
     return res.status(302).redirect(url.longUrl);
   } catch (err) {
     return errorHandler(err, res);
